6c118f7b143d22a57ee3638bce77c1e35dddd0fd
NBGER1 <naorberger@gmail.com>
2021-08-14 15:11:44 +0300

    just weapons

--- ex_1/Assets/Scripts/Gameplay/IDamageable.cs
+++ ex_1/Assets/Scripts/Gameplay/IDamageable.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IDamageable {
+public interface IDamageable
+{
+    #region Functions
 
-  void TakeDamage(float damage);
+    void TakeDamage(float damage);
 
+    #endregion
 }
--- ex_1/Assets/Scripts/Gameplay/Weapon.cs
+++ ex_1/Assets/Scripts/Gameplay/Weapon.cs
@@ -1,3 +1,4 @@
+using GizmoLab.Gameplay;
 using UnityEngine;
 
 
