2851971c6f9a65306f0a89ea805607a66a166532
NBGER1 <naorberger@gmail.com>
2021-08-16 22:20:56 +0300

    moving

--- ex_1/Assets/Scripts/Gameplay/AliensGameCore.cs
+++ ex_1/Assets/Scripts/Gameplay/AliensGameCore.cs
@@ -17,7 +17,7 @@ namespace GizmoLab.Gameplay
 
         public AliensGameCore()
         {
-            //GameplayElements.Instance.Player.IsEnabled = false;
+            GameplayElements.Instance.Player.IsEnabled = true;
         }
 
         #endregion
