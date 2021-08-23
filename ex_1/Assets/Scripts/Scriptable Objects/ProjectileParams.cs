db2ae5b4793697fbf095b993f41dab795b5c56d4
NBGER1 <naorberger@gmail.com>
2021-08-23 08:29:09 +0300

    save

---
+++ ex_1/Assets/Scripts/Scriptable Objects/ProjectileParams.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Scriptable_Objects
+{
+    [CreateAssetMenu(menuName = "Gameplay/Projectile/Params", fileName ="Projectile Params")]
+    public class ProjectileParams : ScriptableObject
+    {
+        #region Editor
+
+        [Range(0f,2f)]
+        [SerializeField] private float _speed;
+
+        #endregion
+
+        #region Properties
+
+        public float Speed => _speed;
+
+        #endregion
+    }
+}
